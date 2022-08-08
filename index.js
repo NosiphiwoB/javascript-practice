548ca2f2b2be07d24a21703e6716d93df2d8c15b
NosiphiwoB <biyelanosiphiwo@gmail.com>
2022-08-08 11:49:26 +0200

    javascript practise

---
+++ index.js
@@ -0,0 +1,10 @@
+const returnName = () => {
+    const myName = "Nosiphiwo"
+ document.getElementById("name").innerHTML= myName
+}
+
+const clearName = () => {
+    // const myName = " "
+document.getElementById("name").innerHTML = ''
+}
+
